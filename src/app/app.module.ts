7c84e87aefc35d443165a8587bbc0dbcc8785be5
Surya <surya.yerramsetti@thomsonreuters.com>
2017-11-23 14:11:16 -0600

    initial commit with angular material :)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MaterialModule,  MatNativeDateModule } from '@angular/material';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+
 
 
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, BrowserAnimationsModule, MatFormFieldModule, MatInputModule, MatDatepickerModule,  MatNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
