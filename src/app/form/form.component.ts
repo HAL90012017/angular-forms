69b3e863aaaa083d6a9864ca35c1f7d2fb619687
Surya <surya.yerramsetti@thomsonreuters.com>
2017-11-23 20:02:59 -0600

    template driven forms done

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -12,4 +12,22 @@ export class FormComponent implements OnInit {
   ngOnInit() {
   }
 
+<<<<<<< Updated upstream
+=======
+ public getSubmittedEmployeeInfo () {
+   return JSON.stringify(this.model);
+ }
+
+ public submit(employeeDetailsForm) {
+   if (employeeDetailsForm.valid) {
+      this.submitted = true;
+   } else {
+     for (const i in employeeDetailsForm.controls) {
+           if (employeeDetailsForm.controls[i]) {
+               employeeDetailsForm.controls[i].markAsTouched();
+           }
+       }
+   }
+ }
+>>>>>>> Stashed changes
 }
